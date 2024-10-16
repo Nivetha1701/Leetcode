178866dcba807b2e1dbe5e4340b7f13feedf3f66
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2024-10-16 10:13:10 +0530

    Time: 3 ms (51.07%), Space: 40.6 MB (47.38%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -7,9 +7,11 @@ class Solution {
             al.add(i);
         }
         int curr=0;
+    // k-1 is because we need to move k-1 position to reach kth friend
+    // % al.size() is because when we reach end we again need to start from 1st pos
         while(al.size()>1)
         {
-            curr=(curr+k-1) % al.size();
+            curr = (curr+k-1) % al.size();
             al.remove(curr);
         }   
         return al.get(0);
