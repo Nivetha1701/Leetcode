bdf8a3ced8960228ded41cf40f92dc090e993a2f
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2024-10-23 22:28:08 +0530

    Time: 1 ms (97.11%), Space: 43.4 MB (37.87%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,25 +1,54 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
         int n=nums.length;
-        int arr[]=new int[n];
-        int j=0;
+        int max1=Integer.MIN_VALUE;
+        int max2=Integer.MIN_VALUE;
+        int max3=Integer.MIN_VALUE;
+        boolean hasmax3=false;
         for(int i=0;i<n;i++)
         {
-            if(!set.contains(nums[i]))
+            max1=Math.max(max1,nums[i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>max2 && nums[i]<max1)
             {
-                set.add(nums[i]);
-                arr[j++]=nums[i];
+                max2=nums[i];
             }
         }
-        Arrays.sort(arr,0,j);
-        if(j<3)
+        for(int i=0;i<n;i++)
         {
-            return arr[j-1];
+            if(nums[i]>=max3 && nums[i]<max2 && nums[i]<max1)
+            {
+                max3=nums[i];
+                hasmax3=true;
+            }
         }
-        else 
+        if(hasmax3)
         {
-            return arr[j-3];
+            return max3;
         }
+        return max1;
+        // HashSet<Integer> set=new HashSet<>();
+        // int n=nums.length;
+        // int arr[]=new int[n];
+        // int j=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(!set.contains(nums[i]))
+        //     {
+        //         set.add(nums[i]);
+        //         arr[j++]=nums[i];
+        //     }
+        // }
+        // Arrays.sort(arr,0,j);
+        // if(j<3)
+        // {
+        //     return arr[j-1];
+        // }
+        // else 
+        // {
+        //     return arr[j-3];
+        // }
     }
 }
