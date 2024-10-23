d0b1b1c9bf65ef0993bc1905d810e7b3976e9fa3
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2024-10-23 23:00:05 +0530

    Time: 2 ms (66.2%), Space: 59.7 MB (13.75%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.java
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -15,20 +15,6 @@ class Solution {
             }
         }
         return increase || decrease;
-        // for(int i=0;i<n;i++)
-        // {
-        //     for(int j=i+1;j<n;j++)
-        //     {
-        //         if(nums[i]>nums[j])
-        //         {
-        //             increase=false;
-        //         }
-        //         if(nums[i]<nums[j])
-        //         {
-        //             decrease=false;
-        //         }
-        //     }
-        // }
-        // return increase || decrease;
+        
     }
 }
