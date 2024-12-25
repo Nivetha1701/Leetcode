191691e0a4642eaf3b2044ff78c84e2d64cf29cb
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2024-12-25 12:16:47 +0530

    Time: 3 ms (41.39%), Space: 42.9 MB (17.57%) - LeetHub

--- 3408-count-the-number-of-special-characters-i/3408-count-the-number-of-special-characters-i.java
+++ 3408-count-the-number-of-special-characters-i/3408-count-the-number-of-special-characters-i.java
@@ -1,29 +1,52 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
+        HashSet<Character> lower=new HashSet<>();
+        HashSet<Character> upper=new HashSet<>();
         int count=0;
-        String upper="";
-        String lower="";
-        HashSet<Character> set=new HashSet<>();
-        for(int i=0;i<word.length();i++){
-            char c=word.charAt(i);
-            if(!set.contains(c)){
-                set.add(c);
-                if(Character.isUpperCase(c)){
-                    upper+=c;
-                } else{
-                    lower+=c;
-                }
+        for(int i=0;i<word.length();i++)
+        {
+            char ch=word.charAt(i);
+            if(Character.isUpperCase(ch))
+            {
+                upper.add(Character.toLowerCase(ch));
+            }
+            else
+            {
+                lower.add(ch);
             }
         }
-        for(int i=0;i<lower.length();i++){
-            char ch=lower.charAt(i);
-            for(int j=0;j<upper.length();j++){
-                char c=upper.charAt(j);
-                if(c==Character.toUpperCase(ch)){
-                    count++;
-                }
+        for(char c:lower)
+        {
+            if(upper.contains(c))
+            {
+                count++;
             }
         }
         return count;
+        // int count=0;
+        // String upper="";
+        // String lower="";
+        // HashSet<Character> set=new HashSet<>();
+        // for(int i=0;i<word.length();i++){
+        //     char c=word.charAt(i);
+        //     if(!set.contains(c)){
+        //         set.add(c);
+        //         if(Character.isUpperCase(c)){
+        //             upper+=c;
+        //         } else{
+        //             lower+=c;
+        //         }
+        //     }
+        // }
+        // for(int i=0;i<lower.length();i++){
+        //     char ch=lower.charAt(i);
+        //     for(int j=0;j<upper.length();j++){
+        //         char c=upper.charAt(j);
+        //         if(c==Character.toUpperCase(ch)){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
     }
 }
