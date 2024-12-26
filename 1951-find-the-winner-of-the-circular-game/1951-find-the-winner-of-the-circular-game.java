575990bacd990b87f458e79f143035476b6368e3
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2024-12-26 14:02:15 +0530

    Time: 0 ms (100%), Space: 40.7 MB (48.35%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,19 +1,10 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-
-        LinkedList<Integer> al=new LinkedList<>();
-        for(int i=1;i<=n;i++)
+        int winner=0;
+        for(int i=2;i<=n;i++)
         {
-            al.add(i);
+            winner=(winner+k)%i;
         }
-        int curr=0;
-    // k-1 is because we need to move k-1 position to reach kth friend
-    // % al.size() is because when we reach end we again need to start from 1st pos
-        while(al.size()>1)
-        {
-            curr = (curr+k-1) % al.size();
-            al.remove(curr);
-        }   
-        return al.get(0);
+        return winner+1;
     }
 }
