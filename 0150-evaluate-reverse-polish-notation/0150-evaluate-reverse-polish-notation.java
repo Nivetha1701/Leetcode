8cf47a5e8a7666da2b7edff2e670922cbf687a20
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-02-18 14:07:15 +0530

    Time: 5 ms (97.57%), Space: 44.8 MB (52.34%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,16 +1,12 @@
 class Solution {
-    private boolean isDigit(String token){
-        try{
-            Integer.parseInt(token);
-            return true;
-        } catch(Exception e){
-            return false;
-        }
+    private boolean isOperator(String token){
+        return token.equals("+") || token.equals("-") || token.equals("*") 
+        || token.equals("/");
     }
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack=new Stack<>();
         for(String token:tokens){
-            if(isDigit(token)){
+            if(!isOperator(token)){
                 stack.push(Integer.parseInt(token));
             } else {
                 int b=stack.pop();
@@ -30,3 +26,35 @@ class Solution {
         return stack.peek();
     }
 }
+// class Solution {
+//     private boolean isDigit(String token){
+//         try{
+//             Integer.parseInt(token);
+//             return true;
+//         } catch(Exception e){
+//             return false;
+//         }
+//     }
+//     public int evalRPN(String[] tokens) {
+//         Stack<Integer> stack=new Stack<>();
+//         for(String token:tokens){
+//             if(isDigit(token)){
+//                 stack.push(Integer.parseInt(token));
+//             } else {
+//                 int b=stack.pop();
+//                 int a=stack.pop();
+
+//                 if(token.equals("+")){
+//                     stack.push(a+b);
+//                 } else if(token.equals("-")){
+//                     stack.push(a-b);
+//                 } else if(token.equals("*")){
+//                     stack.push(a*b);
+//                 } else if(token.equals("/")){
+//                     stack.push(a/b);
+//                 }
+//             }
+//         }
+//         return stack.peek();
+//     }
+// }
