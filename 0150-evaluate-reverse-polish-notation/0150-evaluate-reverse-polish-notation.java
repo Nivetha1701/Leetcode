c7896c93a83b3b9544ad357ca4e39590d0f2613b
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-02-18 14:12:09 +0530

    Time: 5 ms (97.57%), Space: 44.9 MB (30.83%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -12,17 +12,47 @@ class Solution {
                 int b=stack.pop();
                 int a=stack.pop();
 
-                switch(token){
-                case "+": stack.push(a+b); break;
-                case "-": stack.push(a-b); break;
-                case "*": stack.push(a*b); break;
-                case "/": stack.push(a/b); break;
+                if(token.equals("+")){
+                    stack.push(a+b);
+                } else if(token.equals("-")){
+                    stack.push(a-b);
+                } else if(token.equals("*")){
+                    stack.push(a*b);
+                } else if(token.equals("/")){
+                    stack.push(a/b);
                 }
             }
         }
         return stack.peek();
     }
 }
+// class Solution {
+//     private boolean isOperator(String token){
+//         return token.equals("+") || token.equals("-") || token.equals("*") 
+//         || token.equals("/");
+//     }
+//     public int evalRPN(String[] tokens) {
+//         Stack<Integer> stack=new Stack<>();
+//         for(String token:tokens){
+//             if(!isOperator(token)){
+//                 stack.push(Integer.parseInt(token));
+//             } else {
+//                 int b=stack.pop();
+//                 int a=stack.pop();
+
+//                 switch(token){
+//                 case "+": stack.push(a+b); break;
+//                 case "-": stack.push(a-b); break;
+//                 case "*": stack.push(a*b); break;
+//                 case "/": stack.push(a/b); break;
+//                 }
+//             }
+//         }
+//         return stack.peek();
+//     }
+// }
+
+
 // class Solution {
 //     private boolean isDigit(String token){
 //         try{
