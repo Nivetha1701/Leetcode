2540b74167c071bf9245b53d67f8b7f949590889
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-03-12 21:55:09 +0530

    Time: 1 ms (50.69%), Space: 44.9 MB (61.46%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -1,24 +1,38 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        ArrayList<Integer> even=new ArrayList<>();
-        ArrayList<Integer> odd=new ArrayList<>();
-        int ans[]=new int[nums.length];
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]%2==0)
-                even.add(nums[i]);
-            else
-                odd.add(nums[i]);
+        int n=nums.length;
+        int j=0;
+        int ans[]=new int[n];
+        for(int num:nums){
+            if(num%2==0){
+                ans[j++]=num;
+            }
         }
-        Collections.sort(even);
-        for(int i=0;i<odd.size();i++)
-        {
-            even.add(odd.get(i));
-        }
-        for(int i=0;i<nums.length;i++)
-        {
-            ans[i]=even.get(i);
+        for(int num:nums){
+            if(num%2!=0){
+                ans[j++]=num;
+            }
         }
         return ans;
+        // ArrayList<Integer> even=new ArrayList<>();
+        // ArrayList<Integer> odd=new ArrayList<>();
+        // int ans[]=new int[nums.length];
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(nums[i]%2==0)
+        //         even.add(nums[i]);
+        //     else
+        //         odd.add(nums[i]);
+        // }
+        // Collections.sort(even);
+        // for(int i=0;i<odd.size();i++)
+        // {
+        //     even.add(odd.get(i));
+        // }
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     ans[i]=even.get(i);
+        // }
+        // return ans;
     }
 }
