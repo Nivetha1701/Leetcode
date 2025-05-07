2c7123360a1ff9d1fb96dbf86169e0442ac0c439
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-05-07 12:11:35 +0530

    Time: 5 ms (35.54%), Space: 42.4 MB (27.97%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
@@ -2,28 +2,39 @@ import java.util.*;
 
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
-        ArrayList<String> al=new ArrayList<>();
-        ArrayList<String> ans=new ArrayList<>();
-        String[] str1 = s1.split(" ");
-        String[] str2 = s2.split(" ");
-        for(String num:str1){
-            al.add(num);
-        }
-        for(String num:str2){
-            al.add(num);
-        }
-        for(String word:al){
-            int count=0;
-            for(String w:al){
-                if(word.equals(w)){
-                    count++;
-                }
-            }
-            if(count==1){
-                ans.add(word);
+        HashSet<String> once=new HashSet<>();
+        HashSet<String> multiple=new HashSet<>();
+        for(String word : (s1+" "+s2).split(" ")){
+            if(!once.add(word)){
+                multiple.add(word);
             }
         }
-        String res[]=ans.toArray(new String[0]);
-        return res;
+        once.removeAll(multiple);
+        return once.toArray(new String[0]);
+
+
+        // ArrayList<String> al=new ArrayList<>();
+        // ArrayList<String> ans=new ArrayList<>();
+        // String[] str1 = s1.split(" ");
+        // String[] str2 = s2.split(" ");
+        // for(String num:str1){
+        //     al.add(num);
+        // }
+        // for(String num:str2){
+        //     al.add(num);
+        // }
+        // for(String word:al){
+        //     int count=0;
+        //     for(String w:al){
+        //         if(word.equals(w)){
+        //             count++;
+        //         }
+        //     }
+        //     if(count==1){
+        //         ans.add(word);
+        //     }
+        // }
+        // String res[]=ans.toArray(new String[0]);
+        // return res;
     }
 }
