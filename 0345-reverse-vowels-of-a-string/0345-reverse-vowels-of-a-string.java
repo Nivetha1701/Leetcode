b5ba31ad94f2357db40180b5e913f2cbfe3010e1
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-05-08 23:16:11 +0530

    Time: 8 ms (18.87%), Space: 44.9 MB (84.73%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,16 +1,15 @@
 class Solution {
     public String reverseVowels(String s) {
         Stack<Character> st=new Stack<>();
-        HashSet<Character> set=new HashSet<>(Arrays.asList('a','e','i','o','u',
-        'A','E','I','O','U'));
+        String vowel = "aeiouAEIOU";
         for(char c:s.toCharArray()){
-            if(set.contains(c)){
+            if(vowel.indexOf(c)!=-1){
                 st.push(c);
             }
         }
         StringBuilder sb=new StringBuilder();
         for(char c:s.toCharArray()){
-            if(set.contains(c)){
+            if(vowel.indexOf(c)!=-1){
                 sb.append(st.pop());
             } else{
                 sb.append(c);
@@ -20,15 +19,16 @@ class Solution {
     }
 }
         // Stack<Character> st=new Stack<>();
-        // String vowel = "aeiouAEIOU";
+        // HashSet<Character> set=new HashSet<>(Arrays.asList('a','e','i','o','u',
+        // 'A','E','I','O','U'));
         // for(char c:s.toCharArray()){
-        //     if(vowel.indexOf(c)!=-1){
+        //     if(set.contains(c)){
         //         st.push(c);
         //     }
         // }
         // StringBuilder sb=new StringBuilder();
         // for(char c:s.toCharArray()){
-        //     if(vowel.indexOf(c)!=-1){
+        //     if(set.contains(c)){
         //         sb.append(st.pop());
         //     } else{
         //         sb.append(c);
