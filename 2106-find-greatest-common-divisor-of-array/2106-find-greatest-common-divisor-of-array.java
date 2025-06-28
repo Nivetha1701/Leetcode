c6960805944bb91644cd994c359ed1018a5bfdd4
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-06-28 11:38:32 +0530

    Time: 2 ms (38.04%), Space: 43.5 MB (41.04%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.java
@@ -5,16 +5,17 @@ class Solution {
         Arrays.sort(nums);
         int small=nums[0];
         int large=nums[nums.length-1];
-        return BigInteger.valueOf(small).gcd(BigInteger.valueOf(large)).intValue();
+        return gcd(small,large);
     }
+    public int gcd(int a,int b){
+        if(b==0) return a;
+        return gcd(b,a%b);
+    }
+    
     // public int findGCD(int[] nums) {
     //     Arrays.sort(nums);
     //     int small=nums[0];
     //     int large=nums[nums.length-1];
-    //     return gcd(small,large);
-    // }
-    // public int gcd(int a,int b){
-    //     if(b==0) return a;
-    //     return gcd(b,a%b);
+    //     return BigInteger.valueOf(small).gcd(BigInteger.valueOf(large)).intValue();
     // }
 }
