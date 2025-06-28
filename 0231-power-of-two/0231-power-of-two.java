2ba93d53a75a6a8b3b2ec061ffff99e115214954
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-06-28 12:13:44 +0530

    Time: 0 ms (100%), Space: 41 MB (37.95%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         // Approach :2
-        if(n==1) return true;
-        if(n<=0 || n%2!=0) return false;
-        return isPowerOfTwo(n/2);
-        
+        // if(n==1) return true;
+        // if(n<=0 || n%2!=0) return false;
+        // return isPowerOfTwo(n/2);
+
         // Approach :1
-        // if(n>0 && (n&(n-1))==0)
-        // {
-        //     return true;
-        // }
-        // return false;
+        if(n>0 && (n&(n-1))==0)
+        {
+            return true;
+        }
+        return false;
     }
 }
