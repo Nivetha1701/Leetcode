e4de31aae50e29161e18fbda836f39ac8087efa0
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-06-29 23:50:14 +0530

    Time: 1 ms (98.71%), Space: 42.4 MB (95.95%) - LeetHub

--- 3914-check-if-any-element-has-prime-frequency/3914-check-if-any-element-has-prime-frequency.java
+++ 3914-check-if-any-element-has-prime-frequency/3914-check-if-any-element-has-prime-frequency.java
@@ -12,7 +12,6 @@ class Solution {
         return false;
     }
     public static boolean isPrime(int n){
-        if(n<=1) return false;
         for(int i=2;i*i<=n;i++){
             if(n%i==0){
                 return false;
