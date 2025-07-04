bed3d80bcb58c6466a2828445a22e400031e44a5
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-07-04 18:25:39 +0530

    Time: 18 ms (5.52%), Space: 42.3 MB (5.89%) - LeetHub

--- 1289-day-of-the-week/1289-day-of-the-week.java
+++ 1289-day-of-the-week/1289-day-of-the-week.java
@@ -1,12 +1,24 @@
-import java.time.*;
-
+import java.util.Calendar;
 class Solution {
-    public String dayOfTheWeek(int day, int month, int year) {
-        LocalDate date=LocalDate.of(year,month,day);
-        DayOfWeek d=date.getDayOfWeek();
-        return capitalize(d.toString().toLowerCase());
-    }
-    public static String capitalize(String str){
-        return str.substring(0,1).toUpperCase()+str.substring(1);
+    public String dayOfTheWeek(int day,int month,int year){
+        Calendar cal=Calendar.getInstance();
+        cal.set(year,month-1,day);
+        int dayIndex=cal.get(Calendar.DAY_OF_WEEK);
+        String days[]={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+        return days[dayIndex-1];
     }
 }
+
+
+// import java.time.*;
+
+// class Solution {
+//     public String dayOfTheWeek(int day, int month, int year) {
+//         LocalDate date=LocalDate.of(year,month,day);
+//         DayOfWeek d=date.getDayOfWeek();
+//         return capitalize(d.toString().toLowerCase());
+//     }
+//     public static String capitalize(String str){
+//         return str.substring(0,1).toUpperCase()+str.substring(1);
+//     }
+// }
