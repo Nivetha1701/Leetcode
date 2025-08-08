41f8b2c7b96fef32b0c18095ed5c7cc1bee42ec8
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-08-08 23:28:52 +0530

    Time: 2 ms (90.35%), Space: 45.3 MB (79.86%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
@@ -1,26 +1,49 @@
 
 class Solution {
     public List<Integer> luckyNumbers(int[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int rowMin[]=new int[m];
-        int colMax[]=new int[n];
-        Arrays.fill(rowMin, Integer.MAX_VALUE);
-        Arrays.fill(colMax, Integer.MIN_VALUE);
-        for(int i=0;i<m;i++){
-            for(int j =0;j<n;j++){
-                rowMin[i]=Math.min(rowMin[i], matrix[i][j]);
-                colMax[j]=Math.max(colMax[j],matrix[i][j]);
-            }
-        }
         ArrayList<Integer> al=new ArrayList<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==rowMin[i] && matrix[i][j]==colMax[j]){
-                    al.add(matrix[i][j]);
+        for(int i=0;i<matrix.length;i++){
+            int minRow=matrix[i][0];
+            int colIndex=0;
+            for(int j=1;j<matrix[i].length;j++){
+                if(matrix[i][j]<minRow){
+                    minRow=matrix[i][j];
+                    colIndex=j;
                 }
+            }
+            boolean maxCol=true;
+            for(int k=0;k<matrix.length;k++){
+                if(matrix[k][colIndex]>minRow){
+                    maxCol=false;
+                    break;
+                }
+            }
+            if(maxCol){
+                al.add(minRow);
             }
         }
         return al;
     }
 }
+
+        // int m=matrix.length;
+        // int n=matrix[0].length;
+        // int rowMin[]=new int[m];
+        // int colMax[]=new int[n];
+        // Arrays.fill(rowMin, Integer.MAX_VALUE);
+        // Arrays.fill(colMax, Integer.MIN_VALUE);
+        // for(int i=0;i<m;i++){
+        //     for(int j =0;j<n;j++){
+        //         rowMin[i]=Math.min(rowMin[i], matrix[i][j]);
+        //         colMax[j]=Math.max(colMax[j],matrix[i][j]);
+        //     }
+        // }
+        // ArrayList<Integer> al=new ArrayList<>();
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(matrix[i][j]==rowMin[i] && matrix[i][j]==colMax[j]){
+        //             al.add(matrix[i][j]);
+        //         }
+        //     }
+        // }
+        // return al;
