bfda5c5d7ba77f09f2ee9507db7c927c371ddcab
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-08-15 11:36:55 +0530

    Time: 15 ms (20.38%), Space: 45.7 MB (12.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase();
-        String s1="";
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if((c>='a' && c<='z') || (c>='0' && c<='9')){
-                s1+=c;
-            }
-        }
-        StringBuilder str=new StringBuilder(s1);
-        String pal=str.reverse().toString();
-        if(s1.equals(pal)){
-            return true;
-        }
-        return false;
+        
+        String str=s.replaceAll("[^A-Za-z0-9]","").toLowerCase();
+        return str.equals(new StringBuilder(str).reverse().toString());
+
+        // s=s.toLowerCase();
+        // String s1="";
+        // for(int i=0;i<s.length();i++){
+        //     char c=s.charAt(i);
+        //     if((c>='a' && c<='z') || (c>='0' && c<='9')){
+        //         s1+=c;
+        //     }
+        // }
+        // StringBuilder str=new StringBuilder(s1);
+        // String pal=str.reverse().toString();
+        // if(s1.equals(pal)){
+        //     return true;
+        // }
+        // return false;
     }
 }
