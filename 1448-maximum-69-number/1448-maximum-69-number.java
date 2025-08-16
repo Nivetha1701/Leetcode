e9f68afb88b6ad42742535e50a9692aa7c855a74
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-08-16 15:12:27 +0530

    Time: 0 ms (100%), Space: 40.3 MB (94.76%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.java
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -1,16 +1,25 @@
 class Solution {
     public int maximum69Number (int num) {
-        String s=Integer.toString(num);
-        int count=0;
-        String ans="";
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='6' && count<1){
-                ans+="9";
-                count++;
-            } else{
-                ans+=s.charAt(i);
+        char digits[]=Integer.toString(num).toCharArray();
+        for(int i=0;i<digits.length;i++){
+            if(digits[i]=='6'){
+                digits[i]='9';
+                break;
             }
         }
-        return Integer.parseInt(ans);
+        return Integer.parseInt(new String(digits));
+        
+        // String s=Integer.toString(num);
+        // int count=0;
+        // String ans="";
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)=='6' && count<1){
+        //         ans+="9";
+        //         count++;
+        //     } else{
+        //         ans+=s.charAt(i);
+        //     }
+        // }
+        // return Integer.parseInt(ans);
     }
 }
