c15c2694f5c1bc336ddd30b3225dc9764b7e0b9b
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-08-31 10:27:13 +0530

    Time: 1 ms (82.59%), Space: 41.6 MB (81.59%) - LeetHub

--- 2668-form-smallest-number-from-two-digit-arrays/2668-form-smallest-number-from-two-digit-arrays.java
+++ 2668-form-smallest-number-from-two-digit-arrays/2668-form-smallest-number-from-two-digit-arrays.java
@@ -1,23 +1,35 @@
 class Solution {
     public int minNumber(int[] nums1, int[] nums2) {
-        int min=Integer.MAX_VALUE;
-        for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                String s="";
-                if(nums1[i]==nums2[j]){
-                    s+=String.valueOf(nums1[i]);
-                } else{
-                    if(nums1[i]<nums2[j]){
-                        s+=String.valueOf(nums1[i]);
-                        s+=String.valueOf(nums2[j]);
-                    } else{
-                        s+=String.valueOf(nums2[j]);
-                        s+=String.valueOf(nums1[i]);
-                    }
-                }
-                min=Math.min(Integer.parseInt(s), min);
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        for(int a:nums1){
+            for(int b:nums2){
+                if(a==b) return a;
             }
         }
-        return min;
+        return Math.min(nums1[0]*10 + nums2[0], nums2[0]*10 + nums1[0]);
     }
 }
+// class Solution {
+//     public int minNumber(int[] nums1, int[] nums2) {
+//         int min=Integer.MAX_VALUE;
+//         for(int i=0;i<nums1.length;i++){
+//             for(int j=0;j<nums2.length;j++){
+//                 String s="";
+//                 if(nums1[i]==nums2[j]){
+//                     s+=String.valueOf(nums1[i]);
+//                 } else{
+//                     if(nums1[i]<nums2[j]){
+//                         s+=String.valueOf(nums1[i]);
+//                         s+=String.valueOf(nums2[j]);
+//                     } else{
+//                         s+=String.valueOf(nums2[j]);
+//                         s+=String.valueOf(nums1[i]);
+//                     }
+//                 }
+//                 min=Math.min(Integer.parseInt(s), min);
+//             }
+//         }
+//         return min;
+//     }
+// }
