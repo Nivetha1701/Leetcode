4b77505f9d8277f4e9597f115b3b9c5d56eb8d41
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-09-06 11:12:29 +0530

    Time: 2 ms (98.87%), Space: 46.7 MB (83.58%) - LeetHub

--- 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
@@ -1,21 +1,37 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
-        int ans1[]=new int[nums.length/2];
-        int ans2[]=new int[nums.length/2];
-        int i1=0,i2=0;
+        int res[]=new int[nums.length];
+        int even=0, odd=1;
         for(int i=0;i<nums.length;i++){
             if(nums[i]%2==0){
-                ans1[i1++]=nums[i];
+                res[even]=nums[i];
+                even+=2;
             } else{
-                ans2[i2++]=nums[i];
+                res[odd]=nums[i];
+                odd+=2;
             }
-        }
-        int res[]=new int[nums.length];
-        int index=0;
-        for(int i=0;i<ans2.length;i++){
-            res[index++]=ans1[i];
-            res[index++]=ans2[i];
         }
         return res;
     }
 }
+// class Solution {
+//     public int[] sortArrayByParityII(int[] nums) {
+//         int ans1[]=new int[nums.length/2];
+//         int ans2[]=new int[nums.length/2];
+//         int i1=0,i2=0;
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]%2==0){
+//                 ans1[i1++]=nums[i];
+//             } else{
+//                 ans2[i2++]=nums[i];
+//             }
+//         }
+//         int res[]=new int[nums.length];
+//         int index=0;
+//         for(int i=0;i<ans2.length;i++){
+//             res[index++]=ans1[i];
+//             res[index++]=ans2[i];
+//         }
+//         return res;
+//     }
+// }
