6289e2d1d3baf46ea481380cc8f6d747aeb005b2
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-09-06 11:08:43 +0530

    Time: 3 ms (48.05%), Space: 46.5 MB (95.69%) - LeetHub

---
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int ans1[]=new int[nums.length/2];
+        int ans2[]=new int[nums.length/2];
+        int i1=0,i2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                ans1[i1++]=nums[i];
+            } else{
+                ans2[i2++]=nums[i];
+            }
+        }
+        int res[]=new int[nums.length];
+        int index=0;
+        for(int i=0;i<ans2.length;i++){
+            res[index++]=ans1[i];
+            res[index++]=ans2[i];
+        }
+        return res;
+    }
+}
