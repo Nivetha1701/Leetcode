b935d74d18754dc4979cde7330b7e82deb263dd5
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-09-11 10:12:51 +0530

    Time: 27 ms (81.24%), Space: 46 MB (78.03%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -1,30 +1,58 @@
 class Solution {
     public String sortVowels(String s) {
-        String v="";
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(isVowel(c)){
-                v+=c;
+        StringBuilder vowels = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (isVowel(c)) {
+                vowels.append(c);
             }
         }
-        char ch[]=v.toCharArray();
-        int index=0;
+        char[] ch = vowels.toString().toCharArray();
         Arrays.sort(ch);
-        String res="";
-        for(int i=0;i<s.length();i++){
-            if(isVowel(s.charAt(i))){
-                res+=ch[index++];
+        int index = 0;
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (isVowel(c)) {
+                res.append(ch[index++]);  
             } else {
-                res+=s.charAt(i);
+                res.append(c);          
             }
         }
-        return res;
+        return res.toString();
     }
-    public static boolean isVowel(char c){
-        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || 
-        c=='A' || c=='E' || c=='I' || c=='O' || c=='U'){
-            return true;
-        }
-        return false;
+    public static boolean isVowel(char c) {
+        return "aeiouAEIOU".indexOf(c) != -1;
     }
 }
+
+// class Solution {
+//     public String sortVowels(String s) {
+//         String v="";
+//         for(int i=0;i<s.length();i++){
+//             char c=s.charAt(i);
+//             if(isVowel(c)){
+//                 v+=c;
+//             }
+//         }
+//         char ch[]=v.toCharArray();
+//         int index=0;
+//         Arrays.sort(ch);
+//         String res="";
+//         for(int i=0;i<s.length();i++){
+//             if(isVowel(s.charAt(i))){
+//                 res+=ch[index++];
+//             } else {
+//                 res+=s.charAt(i);
+//             }
+//         }
+//         return res;
+//     }
+//     public static boolean isVowel(char c){
+//         if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || 
+//         c=='A' || c=='E' || c=='I' || c=='O' || c=='U'){
+//             return true;
+//         }
+//         return false;
+//     }
+// }
