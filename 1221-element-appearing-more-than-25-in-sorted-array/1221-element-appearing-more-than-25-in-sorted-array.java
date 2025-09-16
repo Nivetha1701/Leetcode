d7b4ad5c1d205ae1d2f2777aeaa4b4fca1e9592e
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-09-16 07:06:45 +0530

    Time: 1 ms (56.26%), Space: 45.6 MB (6.4%) - LeetHub

--- 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.java
+++ 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.java
@@ -1,18 +1,35 @@
 class Solution {
     public int findSpecialInteger(int[] arr) {
-        int freq[]=new int[100001];
-        for(int i=0;i<arr.length;i++)
-        {
-            freq[arr[i]]++;
+        int n=arr.length;
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(arr[i]==arr[i-1]){
+                count++;
+            } else{
+                count=1;
+            }
+            if(count> n/4){
+                return arr[i];
+            }
         }
-        int f=0,max=0;
-        for(int i=0;i<arr.length;i++)
-        {
-             if(f<freq[arr[i]]){
-                max=arr[i];
-                f=freq[arr[i]];
-             }
-        }
-        return max;
+        return arr[0];
     }
 }
+// class Solution {
+//     public int findSpecialInteger(int[] arr) {
+//         int freq[]=new int[100001];
+//         for(int i=0;i<arr.length;i++)
+//         {
+//             freq[arr[i]]++;
+//         }
+//         int f=0,max=0;
+//         for(int i=0;i<arr.length;i++)
+//         {
+//              if(f<freq[arr[i]]){
+//                 max=arr[i];
+//                 f=freq[arr[i]];
+//              }
+//         }
+//         return max;
+//     }
+// }
