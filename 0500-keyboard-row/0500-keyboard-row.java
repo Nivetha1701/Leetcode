c4bb5af4b867ea64176f81eb8d1a364f1d5bddd7
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-09-24 18:21:42 +0530

    Time: 2 ms (7.87%), Space: 42 MB (7%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.java
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -1,7 +1,15 @@
 class Solution {
     public String[] findWords(String[] words) {
-        return Arrays.stream(words)
-        .filter(w-> w.toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*"))
-        .toArray(String[]::new);
+        List<String> al=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            if(words[i].toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*")){
+                al.add(words[i]);
+            }
+        }
+        return al.toArray(new String[0]);
+
+        // return Arrays.stream(words)
+        // .filter(w-> w.toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*"))
+        // .toArray(String[]::new);
     }
 }
