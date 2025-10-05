ab435913ed17b5a039699771dc3aa06d60386390
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-10-05 19:19:41 +0530

    Time: 1 ms (45.47%), Space: 57.4 MB (54.5%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -2,7 +2,7 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int n=nums.length;
         k=k%n;   //if k is less than n ok 3%5=3 it will rotate from 3 index
-        //else if k is greater than n then 7%5=2 it will roate from 2 index
+        //else if k is greater than n then 7%5=2 it will rotate from 2 index
         int j=0;
         int ans[]=new int[nums.length];
         for(int i=n-k;i<n;i++)
