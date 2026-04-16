be693444668e5fbd3cab23a7d7ecce30ca77b2ea
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2026-04-17 00:09:17 +0530

    Time: 6 ms (41.11%), Space: 46.5 MB (20.95%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,22 +1,33 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            int count=0;
-            for(int j=0;j<nums.length;j++)
-            {
-                if(nums[i]==nums[j])
-                {
-                    count++;
-                }
-            }
-            if(count==1)
-            {
-                ans=nums[i];
-                break;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for(int num:nums){
+            if(map.get(num)==1){
+                return num;
             }
         }
-        return ans;
+        return -1;
+
+        // int ans=0;
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     int count=0;
+        //     for(int j=0;j<nums.length;j++)
+        //     {
+        //         if(nums[i]==nums[j])
+        //         {
+        //             count++;
+        //         }
+        //     }
+        //     if(count==1)
+        //     {
+        //         ans=nums[i];
+        //         break;
+        //     }
+        // }
+        // return ans;
     }
 }
