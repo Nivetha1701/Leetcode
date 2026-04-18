a09cc13d247919f15b27f6b5d1b28ac0fb2238d6
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2026-04-18 23:58:38 +0530

    Time: 0 ms (100%), Space: 43.6 MB (28.27%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -8,7 +8,7 @@ class Solution {
             }
             digits[i]=0;
         }
-        int res[]=new int[digits.length+1];
+        int res[]=new int[n+1];
         res[0]=1;
         return res;
     }
