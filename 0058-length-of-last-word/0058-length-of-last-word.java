8e99a6d93f4b2a2221aefff186c1ed6a7c4e33ed
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2026-04-18 23:52:18 +0530

    Time: 2 ms (9.15%), Space: 43 MB (55.32%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,18 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        // int len=0;
-        // String str[]=s.split(" ");
-        // return str[str.length-1].length();
-        int count=0;
-        for(int i=s.length()-1;i>=0;i--){
-            if(s.charAt(i)==' ' && count>0){
-                break;
-            }
-            if(s.charAt(i)>='a' && s.charAt(i)<='z' || 
-                     s.charAt(i)>='A' && s.charAt(i)<='Z'){
-                count++;
-            }
-        }
-        return count;
+        String str[]=s.trim().split("\\s+");
+        return str[str.length-1].length();
     }
 }
